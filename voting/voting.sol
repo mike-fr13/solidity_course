e9fc17d529672cad11b525b6d055b4e48ba4d852
ytison <yannick.tison@gmail.com>
2023-05-22 18:59:50 +0200

    fix commentaires

--- voting/voting.sol
+++ voting/voting.sol
@@ -45,6 +45,12 @@ contract Voting is Ownable {
     event ProposalRegistered(uint proposalId);
     event Voted (address voter, uint proposalId);
 
+    /*
+    Ce constructeur initialise le contrat;
+    J'ai pris le choix :
+        - d'ajouter l'adresse de l'admin à la whitelist 
+        - d'initialiser directement le statut du vote à RegisteringVoters
+    */
     constructor (){
         //ajout à la whitelist de l'administrateur déployant le contrat 
         whitelistedVoters[msg.sender] = Voter(true,false,0);
@@ -53,18 +59,19 @@ contract Voting is Ownable {
         currentStatus = WorkflowStatus.RegisteringVoters;
     }
 
+    // check si l'adresse est bien whitelistée
     modifier whitelisted(address _address) {
         require(whitelistedVoters[_address].isRegistered,"l'adresse n'est pas dans la whitelist");
         _;
     }
 
     /*
-        Ce modifier verifie si une adresse est déjà dans la whitelist 
+        Ce modifier verifie si qu'une adresse n'est pas déjà dans la whitelist 
             - si l'address n'est pas présente, le traitement de la fonction appellelante se poursuit
-            - sinon il emet un eveneement AllreadyWhitelisted et skip la fonction appelante
+            - sinon il emet un evenement AllreadyWhitelisted et skip la fonction appelante
 
         C'est utlisé dans le cas de l'import d'adresse à whitlelister avec la fonction bacthAuthorize qui prend une liste d'adresse en entrée.
-        Si une addrese de la liste est déjà whitelisté, on itère quand meme sur la suite de la liste
+        Si une addrese de la liste est déjà whitelisté, on itère quand meme sur la suite de la liste.
     */
     modifier notWhitelisted(address _address) {
         if (!whitelistedVoters[_address].isRegistered) {
@@ -134,7 +141,6 @@ contract Voting is Ownable {
         changeStatusAndEmitEvent(WorkflowStatus.VotesTallied);
     }
 
-
     /*
         - ajoute une adresse à la whitelist si celle ci n'a pas déjà été whitelistée 
         - emet soit un event Whitelisted, soit un event AllreadyWhitelisted (si l'adresse est déjà présente)
@@ -197,6 +203,10 @@ contract Voting is Ownable {
         return (winingProposal);
      }
 
+    /* Ce getter permet de retourner tout le tableau de proposals 
+       Lorsque l'on met uniquement la variable proposals en public, remix nous propose uniquement de recuperer element par element
+       en demandant l'index de l'element en parametre du getter.
+    */
      function getProposals() public view returns (Proposal[] memory) {
          return proposals;
      }
